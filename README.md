e0497622f0405f51f3ac51e87a3a534dfc1b4dc1
João Pedro Bastos <jpedrosantosb@gmail.com>
2023-03-07 20:35:28 -0300

    conclusão projeto

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# js-consumindo-dados-api
-
-Repositório do treinamento "JavaScript: Consumindo e tratando dados de uma API" da Alura Cursos Online.
+# Consumo de API
 
 - JavaScript assíncrono
 - Consumo da API ViaCEP com o fetch API
@@ -8,3 +6,7 @@ Repositório do treinamento "JavaScript: Consumindo e tratando dados de uma API"
 - Async await para criar funções assíncronas
 - Tratamento de possíveis erros de requisição retornados da API
 - Tranformando página estática em dinâmica
+- Seleção de elementos pelo seu id através do getElementById;
+- Inclusão de valores em elementos com a propriedade value;
+
+![a](https://user-images.githubusercontent.com/69656085/223579541-33054f36-878d-4a94-849e-c304abfe71b0.png)
--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(resposta => resposta.json())
-    .then(r => {
-        if(r.erro) {
-            throw Error('Esse cep não existe!')
-        } else console.log(r)
-    })
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log('Processamento concluido!'))
+async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente!');
+        }
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+        var bairro = document.getElementById('bairro');
 
-console.log(consultaCEP)
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+        bairro.value = consultaCEPConvertida.bairro;
+
+
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+        mensagemErro.innerHTML = `<p class="erro__texto">CEP inválido. Tente novamente!</p>`
+        console.log(erro);
+    }
+}
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
